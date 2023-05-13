32d9b8e11227185133367a38ffe8c665b5f52022
Andrés Del Río Henao <jairodh33@gmail.com>
2023-05-12 22:55:47 -0500

    Se han agregado variables de entorno para la usarlas en la conexión a la base de datos

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize('control_restaurante', 'root', 'toor', {
-  host: 'localhost',
-  dialect: 'mysql',
+module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql'
 });
